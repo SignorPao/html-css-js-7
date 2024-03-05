ae6ad3e2c951432a6bd84565060376bdbe00756a
Pavel Belousov <signorpao@gmail.com>
2024-03-05 17:20:26 +0300

    desktop nav done.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,11 +8,14 @@ menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("open");
   const isOpen = navLinks.classList.contains("open");
   menuBtnIcon.setAttribute("class", isOpen ? "ri-close-fill" : "ri-menu-fill");
+  navLinks.scrollTo(0, 0);
 });
 
 // remove menu when mobile menu link click
 link.forEach((l) =>
   l.addEventListener("click", () => {
     navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-fill");
+    navLinks.scrollTo(0, 0);
   })
 );
